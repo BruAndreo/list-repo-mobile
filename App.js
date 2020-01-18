467e2b314b187681749781a3f59f216e228ba536
BruAndreo <brunolino2026@gmail.com>
2020-01-18 11:18:23 -0300

    Estrutura básica do projeto

---
+++ App.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Hello React Native!</Text>
+    </View>
+  );
+}
