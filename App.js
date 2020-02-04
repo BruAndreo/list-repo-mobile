ec26a6d3d13fd6e79e1be469fbafbe176c00bf0f
BruAndreo <brunolino2026@gmail.com>
2020-02-03 22:59:34 -0300

    Adicionando Router e statusbar

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello React Native!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 }
---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return <Container />;
+}
+
+Main.navigationOptions = {
+  title: 'Olá mundo!',
+};
