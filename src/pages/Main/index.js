ed3166e560a725786e03a93f942443ea11e10933
BruAndreo <brunolino2026@gmail.com>
2020-02-10 23:41:01 -0300

    Criando tela de usuários

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
-  title: 'Olá mundo!',
+  title: 'Usuários',
 };
